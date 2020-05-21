419d5203254c3030bc14d9b607f7c6b2c5435d07
Peter Kang <peeterkang@gmail.com>
2020-05-20 17:04:35 -0700

    users component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <UserItem />
+      <div className='container'>
+        <Users />
+      </div>
     </div>
   );
 }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  state = {
-    id: 'id',
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo',
-  };
-
   render() {
     // To avoid repetitive usage of this.state, we will destructure the state object
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
 
     return (
       <div className='card text-center'>
