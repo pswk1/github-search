7a3908c5dfe58119a85266bb3030cf611666d5c5
Peter Kang <peeterkang@gmail.com>
2020-05-28 16:33:57 -0700

    search users moved to context

--- src/App.js
+++ src/App.js
@@ -28,18 +28,6 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  // Search Github Users
-  // if using async/await with arrow function, add it before the parameter
-  const searchUsers = async (text) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`
-    );
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // Get a single Github user
   const getUser = async (username) => {
     setLoading(true);
@@ -90,7 +78,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
