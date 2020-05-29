0cb652ef6b0316552a8e43a255abff2a2af1403c
Peter Kang <peeterkang@gmail.com>
2020-05-29 15:34:12 -0700

    home and unfound components added

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='lead'>Requested Page Not Found</p>
+    </div>
+  );
+};
+
+export default NotFound;
